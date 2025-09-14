b1fdf681104362158bd0fad64e1c5358ad12ba76
Russell Matney <russell.matney@gmail.com>
2025-09-14 00:02:36 -0400

    feat: init cozy runner gym w/ player, platform
    As good a place as any to start a platformer.

---
+++ src/cozy-gunner/player.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+## vars ######################################################################
+
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+@onready var machine: Machine = $Machine
+
+func to_pretty() -> Variant:
+	return {name=name, position=position, machine=machine}
+
+## ready ######################################################################
+
+func _ready() -> void:
+	# Log.debug("Player ready", self)
+	machine.start({actor=self})
+
+	# connect to the machine's state_changed signal
+	machine.transitioned.connect(_on_transit)
+
+func _on_transit(state_name: String) -> void:
+	# Log.debug("Player transitioned to state", state_name)
+	if anim:
+		anim.animation = state_name
+		anim.play()
